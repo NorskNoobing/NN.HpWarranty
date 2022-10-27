fb8555d6c74c2c6c77eb3b7146734fa46095f447
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-10-27 13:05:54 +0200

    Initial commit

---
+++ Build-Module.ps1
@@ -0,0 +1,28 @@
+#Requires -Module ModuleBuilder
+[string]$moduleName = "NN.HpWarranty"
+
+[version]$version = "0.0.1"
+[string]$author = "NorskNoobing"
+[string]$ProjectUri = "https://github.com/$author/$moduleName"
+[string]$releaseNotes = "Initial commit"
+[string]$description = "HP Warranty API integration"
+[array]$tags = @("HP","Warraty","API")
+[version]$PSversion = "7.2"
+
+$manifestSplat = @{
+    "Description" = $description
+    "PowerShellVersion" = $PSversion
+    "Tags" = $tags
+    "ReleaseNotes" = $releaseNotes
+    "Path" = ".\source\$moduleName.psd1"
+    "RootModule" = "$moduleName.psm1"
+    "Author" = $author
+    "ProjectUri" = $ProjectUri
+}
+New-ModuleManifest @manifestSplat
+
+$buildSplat = @{
+    "SourcePath" = ".\source\$moduleName.psd1"
+    "Version" = $version
+}
+Build-Module @buildSplat
