467362b605bea53c2307f45b5b706d40c58f33ae
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2023-01-13 10:27:52 +0100

    Update New-HpwJob.ps1
    * Added examples into get-help
    * Added option to use hashtable inputs. This makes it so you can specify a PN for each SN independently

--- source/Public/New-HpwJob.ps1
+++ source/Public/New-HpwJob.ps1
@@ -1,9 +1,36 @@
 function New-HpwJob {
-    [CmdletBinding()]
+    <#
+    .EXAMPLE
+        @("5CD2022DDS","5CD2022D9Q") | New-HpwJob
+
+        Get an array of serialnumbers without productnumber.
+        Note that this could result in a not found message when retrieving the results.
+    .EXAMPLE
+        @("5CD2022DDS","5CD2022D9Q") | New-HpwJob -pn "2U740AV"
+
+        Get an array of serialnumbers using the same productnumber.
+    .EXAMPLE
+        @(
+        @{
+            "sn" = "5CD2022DDS"
+            "pn" = "2U740AV"
+        },
+        @{
+            "sn" = "5CD2022D9Q"
+            "pn" = "2U740AV"
+        }
+        ) | New-HpwJob -HashtableInput
+
+        You can pipe an array of hashtables into the function with the use of the "-Hashtable" switch
+        in order to set multiple inputs independent of eachother e.g. separate PN for each SN.
+    #>
+    [CmdletBinding(DefaultParameterSetName="Normal inputs")]
     param (
-        [Parameter(Mandatory,ValueFromPipeline,Position=0)]$sn,
-        [string]$pn,
-        [string]$cc
+        [Parameter(ParameterSetName="Normal inputs",Mandatory,ValueFromPipeline,Position=0)]$sn,
+        [Parameter(ParameterSetName="Normal inputs")][string]$pn,
+        [Parameter(ParameterSetName="Normal inputs")][string]$cc,
+        [Parameter(ParameterSetName="Hashtable input",Mandatory,ValueFromPipeline,Position=0)][hashtable]$InputObject,
+        [Parameter(ParameterSetName="Hashtable input")][switch]$HashtableInput
     )
 
     begin {
@@ -11,12 +38,21 @@ function New-HpwJob {
     }
 
     process {
+        switch ($PsCmdlet.ParameterSetName) {
+            "Normal inputs" {
+                $ParamHashtable = $PSBoundParameters
+            }
+            "Hashtable input" {
+                $ParamHashtable = $InputObject
+            }
+        }
+
         $CurrentQueryBody = $null
 
         #Build request Uri
-        $PSBoundParameters.Keys.ForEach({
+        $ParamHashtable.Keys.ForEach({
             [string]$Key = $_
-            [string]$Value = $PSBoundParameters.$key
+            [string]$Value = $ParamHashtable.$key
         
             $CurrentQueryBody = $CurrentQueryBody + @{
                 "$Key" = "$Value"
